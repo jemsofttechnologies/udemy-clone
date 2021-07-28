881c66aafeaa12873e6702f463de16c39267d542
jemsofttechnologies <jemsofttechnologies@gmail.com>
2021-07-28 15:19:42 +0300

    protected pages

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -3,5 +3,5 @@ import expressJwt from "express-jwt";
 export const requireSignIn = expressJwt({
 	getToken: (req, res) => req.cookies.token,
 	secret: process.env.JWT_SECRET,
-	algorithms: ["HS265"],
+	algorithms: ["HS256"],
 }); //
