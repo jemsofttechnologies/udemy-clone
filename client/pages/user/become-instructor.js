ad3c95a0611f672f63c364b3627b8fa54c4273f8
jemsofttechnologies <jemsofttechnologies@gmail.com>
2021-08-12 14:26:10 +0300

    Instructor page

--- client/pages/user/become-instructor.js
+++ client/pages/user/become-instructor.js
@@ -1,3 +1,9 @@
+import {
+	SettingOutlined,
+	UserSwitchOutlined,
+} from "@ant-design/icons";
+import Button from "@material-tailwind/react/Button";
+
 const BecomeInstructor = () => {
 	return (
 		<div className="flex flex-col">
@@ -7,6 +13,31 @@ const BecomeInstructor = () => {
                 font-medium text-white shadow-xl"
 			>
 				<h2 className="">Become Instructor</h2>
+			</div>
+			<div className="flex justify-center">
+				<div className="flex flex-col items-center justify-center mt-10 text-center space-y-6  w-[60%]">
+					<UserSwitchOutlined className="text-7xl" />
+					<h1 className="text-lg font-semibold">
+						Setup payout to publish courses on Udemy
+					</h1>
+					<p className="text-yellow-400">
+						Udemy partners with stripe to transfer earnings to your
+						bank account
+					</p>
+					<Button
+						color="blue"
+						buttonType="outlined"
+						ripple="light"
+						className="capitalize"
+					>
+						<SettingOutlined />
+						Payout Setup
+					</Button>
+					<p className="">
+						You will be redirected to stripe to complete onboarding
+						process
+					</p>
+				</div>
 			</div>
 		</div>
 	);
